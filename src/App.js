575aabc78de2c63209c2fe404ed677b4e050eb4d
Juan Sebastian <stacoolpersonalizados@gmail.com>
2025-02-20 05:22:24 -0500

    Se creó el componente separator con los styles inline

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './containers/Header/Header'
+import Separator from './components/Separator/Separator';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Separator />
     </div>
   );
 }
---
+++ src/components/Separator/Separator.js
@@ -0,0 +1,10 @@
+
+
+function Separator (){
+
+    return (
+        <span style={{ width: '100%', height: '16px', display: 'block'}}></span>
+    )
+}
+
+export default Separator
