822b9f4d3adac36b0cb4e4198c220e0ab66a4156
Andres Felipe Larrotta Pino <andres.larrota@isobar.com>
2025-02-24 18:22:38 -0500

    console.log deleted

--- src/components/Separator/Separator.js
+++ src/components/Separator/Separator.js
@@ -1,5 +1,4 @@
 let Separator = ({ height, color }) => {
-    console.log('cualquier mensaje que necesito mostrara', color)
     let styles = { width: "100%", height: `${height}px`, display: 'block', backgroundColor: `${!color ? "white" : color}` };
     return <span style={styles}></span>;
 };
