bcd29d956678cc20758912fc11ab4cbc7edc02ef
Juan Sebastian <stacoolpersonalizados@gmail.com>
2025-02-26 18:53:03 -0500

    Se corrigen los nombres de las clases

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -3,12 +3,12 @@ import Studiotatto from '../../assets/studio_img.png'
 
 function Service () {
     return (
-        <div className={styles.container}>
-            <div className={styles.container_description}>
-                 <h1 className={styles.letter_description}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.” </h1>
+        <div className={styles.service}>
+            <div className={styles.service_description}>
+                 <h1 className={styles.service_text_description}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.” </h1>
             </div>
-            <div className={styles.container_img}>
-                <img className={styles.container_img_studio} src={Studiotatto} alt='Studio Tattoo'  />
+            <div className={styles.service_img}>
+                <img className={styles.service_img_studio} src={Studiotatto} alt='Studio Tattoo'  />
             </div>
         </div>
     );
